b15dd1b4c82862a24a9d56dc3092e179638b9708
Jason Millen <jasonmillen@jasons-air.home>
2019-10-22 20:33:28 -0400

    Init

---
+++ App.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  ActivityIndicator
+ } from 'react-native';
+
+ import { AuthSession } from 'expo';
+ console.log(AuthSession.getRedirectUrl());
+
+import Search from './src/Components/Search';
+import Listing from './src/Components/Listing';
+//import searchMock from './src/api/spotify/searchMock';
+
+import token from './src/api/spotify/token';
+import search from './src/api/spotify/search';
+
+const PAGE = 20;
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      songs: [],
+      offset: 0,
+      query: 'Shpongle',
+      isFetching: false,
+      isEmpty: false,
+      token: null,
+      isTokenFetching: false
+    };
+  }
+
+  async componentDidMount() {
+    await this.refreshToken();
+    await this.loadNextPage();
+  }
+
+  async loadNextPage() {
+    const { songs, offset, query, token, isFetching, isEmpty } = this.state;
+
+    if (isFetching || isEmpty) {
+      return;
+    }
+
+    this.setState({ isFetching: true });
+
+    const newSongs = await search({
+      offset,
+      limit: PAGE,
+      q: query,
+      token
+    });
+
+    if (newSongs.length === 0) {
+      console.log('No songs found. There may be an error.');
+      this.setState({ isEmpty: true });
+    }
+
+    this.setState({
+      isFetching: false,
+      songs: [...songs, ...newSongs],
+      offset: offset + PAGE
+    });
+  }
+
+  async refreshToken() {
+    this.setState({
+      isTokenFetching: true
+    });
+
+    const newToken = await token();
+
+    this.setState({
+      token: newToken,
+      isTokenFetching: false
+    });
+  }
+
+  handleSearchChange(text) {
+    this.setState({
+      isEmpty: false,
+      query: text,
+      offset: 0,
+      songs: [],
+    }, () => {
+      this.loadNextPage();
+    });
+
+    console.log('search text is', text);
+  }
+
+  async handleEndReached() {
+    await this.loadNextPage();
+  }
+
+  render() {
+    const { songs, query, isFetching } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Search 
+          onChange={text => this.handleSearchChange(text)}
+          text={query}
+        />
+        {
+          (isFetching && songs.length === 0)
+          ? <ActivityIndicator />
+          : <Listing
+            items={songs}
+            onEndReached={() => this.handleEndReached()} />
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+    margin: 10,
+    marginTop: 50
+  },
+});
---
+++ src/Components/Listing.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { FlatList, View, Text } from 'react-native';
+
+import ListItem from './ListItem';
+import Separator from './Separator';
+
+export default ({ items, onEndReached }) => (
+  <FlatList
+    data={items}
+    renderItem={({ item }) => <ListItem item={item}/>}
+    keyExtractor={item => item.id.toString()}
+    ItemSeparatorComponent={() => <Separator />}
+    onEndReached={onEndReached}
+    ListEmptyComponent={() => <Text>No songs.</Text>}
+  />
+);
